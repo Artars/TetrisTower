ff8f73b498510b36ca46ae0e9f498ed6ca918da3
Luiz Victor <luizvictorlrocha@gmail.com>
2018-01-27 09:16:20 -0200

    adds a class to handle all possible blocks

---
+++ Assets/Scripts/BlockFabric.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    classe responsável por armazenar todos os possíveis blocos
+
+ */
+class BlockFabric
+{
+
+    private static GameObject[] prefabArray;
+
+    public static GameObject[] getPrefabArray()
+    {
+        return getPrefabArray;
+    }
+}

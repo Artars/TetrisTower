262552472110c2ee7478492089b3e60151347c9d
Arthur <arsbr.souza@gmail.com>
2018-01-27 17:25:31 -0200

    Deadpit + Vertical Fix

---
+++ Assets/Scripts/Deadpit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deadpit : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collision2D other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}

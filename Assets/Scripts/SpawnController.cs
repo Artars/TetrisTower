8a06c7f8be8210cbeef49507ec7a8cabd9e0823b
Luiz Victor <luizvictorlrocha@gmail.com>
2018-01-27 09:02:05 -0200

    adds spawn controller for better block instantiation

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    o controller que gera spawn
+ */
+public class SpawnController {
+
+    int[] playersIndex;
+    int head;
+    int tail;
+    GameObject[] piecesPrefab;
+    GameObject[] piecesBuffer;
+
+    void Start()
+    {
+        playersIndex = new int[]{0,0};
+        head = 1;
+        tail = 0;
+        piecesBuffer = new GameObject[50];
+        piecesPrefab = BlockFabric::getPrefabArray();
+
+        piecesBuffer[0] =spawnBlock();
+    }
+
+    /*
+        0 para player 1
+        1 para player 0
+     */
+    public GameObject getNextBlock(int player)
+    {
+        ++playersIndex[player];
+        if(playersIndex[player] == piecesBuffer.Length)
+        {
+            playersIndex[player] = 0;
+        }
+        //armazena mais no buffer
+        if(playersIndex[player] == head)
+        {
+            ++head;
+            if(head == piecesBuffer.Length)
+            {
+                head = 0;
+            }
+            piecesBuffer[head] = spawnBlock();
+        }
+
+        return piecesBuffer[playersIndex[player]];
+    }
+
+
+
+    private GameObject spawnBlock()
+    {
+        int randomPrefab = Random.Range(0, piecesPrefab.Length - 1);
+        return GameObject.Instantiate(piecesPrefab[randomPrefab], spawnPlace.position, Quaternion.identity);
+    }
+
+    
+
+}

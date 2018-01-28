de47ab096f32811b11b92a24ab6926ad98eb9732
vinicius <viniciusgf@me.com>
2018-01-28 13:52:05 -0200

    Adicionado Splash Screen

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -8,14 +8,19 @@ public class SplashScreen : MonoBehaviour {
     public string nextMap = "Scenes/MainMenu";
     public float waitTime =3.5f;
     float counter;
+    
+    
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         counter = waitTime;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
+      
+
+        //Load Main Menu after time runs out
         counter -= Time.deltaTime;
         if (counter < 0) {
             SceneManager.LoadScene(nextMap);

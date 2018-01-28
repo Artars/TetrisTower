fc41818153d5bbe1ec77c88f40fcb1b928201cb1
vinicius <viniciusgf@me.com>
2018-01-28 14:48:04 -0200

    Adicionado Power Ups

---
+++ Assets/SpawnFire.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFire : MonoBehaviour
+{
+    public GameObject fireBall;
+    public GameObject fireBall2;
+    bool powerUpSpwaned = false;
+    bool powerUpSpwaned2 = false;
+    fireBall test;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        Transform parent = col.transform.parent;
+        if (parent != null && parent.gameObject.tag.Equals("Block"))
+        {
+            Block blockScript = parent.GetComponent<Block>();
+            if (!blockScript.controlable)
+            {
+                if (!powerUpSpwaned)
+                {
+                    if (blockScript.player == 1)
+                    {
+                        powerUpSpwaned = true;
+                        Instantiate(fireBall, GameObject.Find("Spawn1").transform.position, fireBall.transform.rotation);
+                    }
+                }
+
+                if (!powerUpSpwaned2)
+                {
+                    if (blockScript.player == 2)
+                    {
+                        powerUpSpwaned2 = true;
+                        Instantiate(fireBall2, GameObject.Find("Spawn2").transform.position, fireBall.transform.rotation);
+                    }
+                }
+            }            
+        }
+       
+    }
+}
---
+++ Assets/fireBall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBall : MonoBehaviour {
+
+    public float angle = -45;
+    MoveForwardsAngle mfa;
+    // Use this for initialization
+    void Awake () {
+        mfa = GetComponent<MoveForwardsAngle>();
+        mfa.angle = angle;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        mfa.angle = angle;
+    }
+
+    public static explicit operator fireBall(GameObject v)
+    {
+        throw new NotImplementedException();
+    }
+}

c8af8dad4a2f1146a323503f8e0e022a3c8e885a
Arthur <arsbr.souza@gmail.com>
2018-01-28 01:48:00 -0200

    Add Flag and Line

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour {
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        print("Colidiu " + col.gameObject.name);
+        if (col.gameObject.tag.Equals("Block")) {
+            Block blockScript = col.GetComponent<Block>();
+            if (!blockScript.controlable)
+            {
+                //print("Jogador " + blockScript.player + " atingiu o alvo");
+                SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+                sprite.color = new Color(1, 1, 1, 0.2f);
+            }
+        }
+    }
+}
